649a95eabf58055d42094ac9b8220c601efd1bca
arfanxn <arfan2173@gmail.com>
2024-10-07 12:01:20 +0700

    load external modules

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import external_modules
 import config
 from routes import (web_bp, api_bp)
 
 app = Flask(__name__, instance_relative_config=True)
 config.inject_into_app(app)
+
+# Load external module
+external_modules.load_jaga_aksara_model()
 
 app.register_blueprint(web_bp)
 app.register_blueprint(api_bp)
