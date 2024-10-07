b8f0c8117767d1ec99bd0677243e9a7b8cffee41
arfanxn <arfan2173@gmail.com>
2024-10-07 14:44:57 +0700

    deletions

--- forms/TranslatePegonScriptForm.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, FileField, validators
-from flask_wtf import FlaskForm
-
-class TranslatePegonScriptForm(FlaskForm):
-    photo = FileField('photo', validators=[
-            validators.Regexp(u'^.*\.(jpg|jpeg|png)$', message='Invalid file extension. Only image files with the following extensions are accepted: .jpg, .jpeg, and .png.')
-        ])
